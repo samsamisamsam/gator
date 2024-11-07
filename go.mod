32d85e4ed4bc496497cb31a69d6631141e77a2f1
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-11-07 16:08:16 -0500

    register command

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/samsamisamsam/gator
 
 go 1.23.2
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/google/uuid v1.6.0
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/samsamisamsam/gator/internal/config"
+	"github.com/samsamisamsam/gator/internal/database"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,7 +22,15 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting to db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
@@ -26,10 +38,11 @@ func main() {
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: cli <command> [args...]")
-		os.Exit(1)
+		return
 	}
 
 	cmdName := os.Args[1]
